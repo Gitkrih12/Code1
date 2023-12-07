d970ffbd8041647b94e8bd309e6d80fbc213ec21
GitKrih12 <bathu.prasad@ssspr.com>
2023-12-07 12:16:29 +0530

    new changes

--- Test1.py
+++ Test1.py
@@ -1,9 +1,22 @@
+""""
 class Person:
     def __init__(self,name,age):
         self.name = name
         self.age = age
 
 p1 = Person("Ram", 35)
+
+print(p1.name)
+print(p1.age)
+
+"""
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+p1 = Person("Laxman", 35)
 
 print(p1.name)
 print(p1.age)
